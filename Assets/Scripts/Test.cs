a71c9c8ff3c55f5d2633e66a16d913eb65a9238d
Assanali <assanali.amangeldiyev@my.jcu.edu.au>
2022-12-17 15:14:02 +0800

    Setup polygon package

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("123");
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
