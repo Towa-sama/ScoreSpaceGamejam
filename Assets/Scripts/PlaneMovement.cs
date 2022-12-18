8e7b0ef78077f8c9ff8d654cc69f92a40ab08a71
MisteryDush <i.arifdjanov@yandex.ru>
2022-12-18 15:04:08 +0800

    Fixed plane movement and added new entities to spawn

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0f, 0.2f, 64f);
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(0f, 0.2f, player.transform.position.z + 60f);
+    }
+}
--- Assets/Scripts/SpawnEntities.cs
+++ Assets/Scripts/SpawnEntities.cs
@@ -1,17 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = Unity.Mathematics.Random;
 
 public class SpawnEntities : MonoBehaviour
 {
-    [SerializeField] private GameObject[] entities;
+    [SerializeField] private GameObject[] rocks;
+    [SerializeField] private GameObject[] enemies;
     private const float SPAWN_DELAY = 2f;
     private float spawnTime;
     private Random _random;
     private float planeSizeX;
     private float planeSizeZ;
-    
+
 
     void Start()
     {
@@ -22,6 +25,7 @@ public class SpawnEntities : MonoBehaviour
         planeSizeX = bounds.size.x;
         planeSizeZ = bounds.size.z;
     }
+
     void FixedUpdate()
     {
         if (spawnTime < Time.time)
@@ -33,15 +37,31 @@ public class SpawnEntities : MonoBehaviour
 
     private void Spawn()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 2; i++)
         {
-            int index = _random.NextInt(3);
-            float randomPointX =
-                _random.NextFloat(transform.position.x - planeSizeX, transform.position.x + planeSizeX);
-            float randomPointZ =
-                _random.NextFloat(transform.position.z- planeSizeZ, transform.position.z + planeSizeZ);
-            GameObject lastEntity = Instantiate(entities[index], new Vector3(randomPointX, transform.position.y, randomPointZ), Quaternion.identity);
+            int index = _random.NextInt(rocks.Length);
+            (float randomPointX, float randomPointZ) = GetRandomCoords();
+            GameObject lastEntity = Instantiate(rocks[index],
+                new Vector3(randomPointX, transform.position.y, randomPointZ), Quaternion.identity);
             Destroy(lastEntity, 20f);
         }
+
+        for (int i = 0; i < 3; i++)
+        {
+            int index = _random.NextInt(enemies.Length);
+            (float randomPointX, float randomPointZ) = GetRandomCoords();
+            GameObject lastEntity = Instantiate(enemies[index],
+                new Vector3(randomPointX, transform.position.y, randomPointZ), Quaternion.identity);
+            Destroy(lastEntity, 20f);
+        }
+    }
+
+    private (float randomPointX, float randomPointZ) GetRandomCoords()
+    {
+        float randomPointX =
+            _random.NextFloat(transform.position.x - planeSizeX, transform.position.x + planeSizeX);
+        float randomPointZ =
+            _random.NextFloat(transform.position.z - planeSizeZ, transform.position.z + planeSizeZ);
+        return (randomPointX, randomPointZ);
     }
 }
