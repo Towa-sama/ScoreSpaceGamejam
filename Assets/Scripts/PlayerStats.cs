3d0dfdd1e380567b9fd2cdbb4af237f4fad04141
MisteryDush <i.arifdjanov@yandex.ru>
2022-12-18 17:54:34 +0800

    Add ragdoll physics

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    private int health = 3;
+    private static int health = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private GameObject ragdoll;
+    
 
-    // Update is called once per frame
     void Update()
     {
+        if (health <= 0)
+        {
+            var rg = Instantiate(ragdoll, gameObject.transform.position, Quaternion.identity);
+            ApplyExplosionToRagdoll(rg.transform, 900f, transform.position - new Vector3(0f, 0f, 5f), 10f);
+            Destroy(gameObject);
+        }
     }
 
     public void GetDamage()
     {
         health -= 1;
+    }
+    
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidBody))
+            {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
+            }
+            
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
