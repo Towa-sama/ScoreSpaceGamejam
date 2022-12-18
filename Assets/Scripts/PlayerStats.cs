e196f0076be73905679efd1f88bf4762aee7160e
Assanali <assanali.amangeldiyev@my.jcu.edu.au>
2022-12-18 22:47:24 +0800

    Pushed new changes

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    private static int health = 3;
+    private static int health = 1;
+    private int shieldHealth = 2;
 
     [SerializeField] private GameObject ragdoll;
-    
+    [SerializeField] private TextMeshProUGUI shieldCount;
+    [SerializeField] private GameObject shield;
+    [SerializeField] private GameObject shield2;
+
+    private void Start()
+    {
+        shieldCount.text = "2/2";
+    }
 
     void Update()
     {
@@ -21,7 +31,26 @@ public class PlayerStats : MonoBehaviour
 
     public void GetDamage()
     {
-        health -= 1;
+        if (shieldHealth > 0)
+        {
+            if (shieldHealth == 2)
+            {
+                shield.SetActive(false);
+            }
+
+            if (shieldHealth == 1)
+            {
+                shield2.SetActive(false);
+            }
+            shieldHealth -= 1;
+            shieldCount.text = shieldHealth + "/2";
+        }
+        else
+        {
+            health -= 1;
+        }
+        Debug.Log("current shield health: "+ shieldHealth);
+        Debug.Log("current health: "+ health);
     }
     
     private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
